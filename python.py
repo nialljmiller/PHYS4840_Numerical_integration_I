f2fcd4b3f8cd33572658ab66ef2bde67b97eb1ff
nialljmiller <niall.j.miller@gmail.com>
2025-02-20 13:21:30 -0700

    summarised the whole of python

--- python.py
+++ python.py
@@ -1,4 +1,9 @@
 from numpy.polynomial.legendre import leggauss as legendre_thingy
 
 legendre_roots, weights = legendre_thingy(3)
-print(legendre_roots, weights)
+
+print("Gauss-Legendre Quadrature Points (Roots of P_n(x)):")
+print(legendre_roots)
+
+print("\nWeights for each point:")
+print(weights)
